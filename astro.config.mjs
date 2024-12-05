5be2d7e85a05e578edcd9e83a697bff319316502
Christopher Perry <ozbonus@gmail.com>
2024-12-06 01:22:52 +0800

    Add tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,9 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), sitemap()]
+  integrations: [react(), mdx(), sitemap(), tailwind()]
 });
