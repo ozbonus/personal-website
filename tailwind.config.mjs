8a8eae588b474927d00a93262aec5b2d51d7b3c3
Christopher Perry <ozbonus@gmail.com>
2025-01-07 18:45:28 +0800

    Add more shadow styles.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,21 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "selector",
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     fontFamily: {
       sans: ['"Atkinson Hyperlegible", sans-serif'],
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        "brutal-5": "5px 5px 0px 0px",
+        "brutal-8": "8px 8px 0px 0px",
+        "brutal-12": "12px 12px 0px 0px",
+        "brutal-16": "16px 16px 0px 0px",
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
