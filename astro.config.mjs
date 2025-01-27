d49d53b4978ed0e06ff0d6cd910e13622a60e0f1
Christopher Perry <ozbonus@gmail.com>
2025-01-27 22:09:41 +0800

    Set url.

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,7 @@ export default defineConfig({
     sitemap(),
     tailwind(),
   ],
-  site: "https://hellochris.netlify.app",
+  site: "https://hellochris.dev",
   build: {
     assets: "assets",
   },
